033ea54c1823b1728e4abae2a98fff1baec4200e
Adina Gabriela Iamandi <iamandi.adina95@gmail.com>
2022-12-07 16:38:10 +0100

    Update D4.js

--- D4.js
+++ D4.js
@@ -1,7 +1,7 @@
 const starWarsCharacters = [{
         name: "Luke Skywalker",
         height: "172",
-        mass: "277",
+        mass: 277,
         hair_color: "blond",
         skin_color: "fair",
         eye_color: "blue",
@@ -11,7 +11,7 @@ const starWarsCharacters = [{
     {
         name: "C-3PO",
         height: "167",
-        mass: "75",
+        mass: 75,
         hair_color: "n/a",
         skin_color: "gold",
         eye_color: "yellow",
@@ -21,7 +21,7 @@ const starWarsCharacters = [{
     {
         name: "R2-D2",
         height: "96",
-        mass: "32",
+        mass: 32,
         hair_color: "n/a",
         skin_color: "white, blue",
         eye_color: "red",
@@ -31,7 +31,7 @@ const starWarsCharacters = [{
     {
         name: "Darth Vader",
         height: "202",
-        mass: "136",
+        mass: 136,
         hair_color: "none",
         skin_color: "white",
         eye_color: "yellow",
@@ -41,7 +41,7 @@ const starWarsCharacters = [{
     {
         name: "Leia Organa",
         height: "150",
-        mass: "49",
+        mass: 49,
         hair_color: "brown",
         skin_color: "light",
         eye_color: "brown",
@@ -51,7 +51,7 @@ const starWarsCharacters = [{
     {
         name: "Owen Lars",
         height: "178",
-        mass: "120",
+        mass: 120,
         hair_color: "brown, grey",
         skin_color: "light",
         eye_color: "blue",
@@ -61,7 +61,7 @@ const starWarsCharacters = [{
     {
         name: "Beru Whitesun lars",
         height: "165",
-        mass: "75",
+        mass: 75,
         hair_color: "brown",
         skin_color: "light",
         eye_color: "blue",
@@ -71,7 +71,7 @@ const starWarsCharacters = [{
     {
         name: "R5-D4",
         height: "97",
-        mass: "32",
+        mass: 32,
         hair_color: "n/a",
         skin_color: "white, red",
         eye_color: "red",
@@ -81,7 +81,7 @@ const starWarsCharacters = [{
     {
         name: "Biggs Darklighter",
         height: "183",
-        mass: "84",
+        mass: 84,
         hair_color: "black",
         skin_color: "light",
         eye_color: "brown",
@@ -91,7 +91,7 @@ const starWarsCharacters = [{
     {
         name: "Obi-Wan Kenobi",
         height: "182",
-        mass: "77",
+        mass: 77,
         hair_color: "auburn, white",
         skin_color: "fair",
         eye_color: "blue-gray",
@@ -148,11 +148,19 @@ const eyeColor = {
 
 switch (starWarsCharacters.eye_color) {
     case "blue":
-        eyeColor.blue.push(starWarsCharacters);
+        eyeColor.push(starWarsCharacters);
         break;
     case "yellow":
-        eyeColor.yellow.push(starWarsCharacters);
+        eyeColor.push(starWarsCharacters);
         break;
+    case "brown":
+        eyeColor.push(starWarsCharacters);
+        break;
+    case "red":
+        eyeColor.push(starWarsCharacters);
+        break;
+    case "blue-gray":
+        eyeColor.push(starWarsCharacters);
 }
 
 console.log(eyeColor);
@@ -160,6 +168,14 @@ console.log(eyeColor);
 /* ESERCIZIO 6
    Usa un while loop per calcolare la massa totale dell'equipaggio
   */
+
+let numero = 0;
+let massaTotale = 0;
+while (numero <= 10) {
+    massaTotale = starWarsCharacters.mass + starWarsCharacters.mass;
+}
+
+console.log(massaTotale);
 
 /* ESERCIZIO 7
 
